4e94e60b7cd293aaf264074c2d3df9782bd6b624
Igor-Goncharenko <21gis05ru@gmail.com>
2023-07-15 22:02:02 +0500

    commit

--- scripts/mipt_parser_example.py
+++ scripts/mipt_parser_example.py
@@ -5,11 +5,10 @@ from bs4 import BeautifulSoup
 
 
 # get html text
-req = requests.get("https://priem.mipt.ru/applications/bachelor/000000005_%D0%9F%D1%80%D0%B8%D0%BA%D0%BB%D0%B0%D0%B4%D0%BD%D0%B0%D1%8F%20%D0%BC%D0%B0%D1%82%D0%B5%D0%BC%D0%B0%D1%82%D0%B8%D0%BA%D0%B0%20%D0%B8%20%D0%B8%D0%BD%D1%84%D0%BE%D1%80%D0%BC%D0%B0%D1%82%D0%B8%D0%BA%D0%B0_%D0%91%D1%8E%D0%B4%D0%B6%D0%B5%D1%82.html")
-response = req.content
+response = requests.get("https://priem.mipt.ru/applications/bachelor/000000005_%D0%9F%D1%80%D0%B8%D0%BA%D0%BB%D0%B0%D0%B4%D0%BD%D0%B0%D1%8F%20%D0%BC%D0%B0%D1%82%D0%B5%D0%BC%D0%B0%D1%82%D0%B8%D0%BA%D0%B0%20%D0%B8%20%D0%B8%D0%BD%D1%84%D0%BE%D1%80%D0%BC%D0%B0%D1%82%D0%B8%D0%BA%D0%B0_%D0%91%D1%8E%D0%B4%D0%B6%D0%B5%D1%82.html")
 
 # parse
-soup = BeautifulSoup(response, "lxml")
+soup = BeautifulSoup(response.content, "lxml")
 table = soup.find("table")
 # print(table)
 html_headers = table.find("thead").find_all("th")
